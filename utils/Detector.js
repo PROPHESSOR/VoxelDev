ccecb9fab5dde04382d5429aa33f6ff63eab2fed
PROPHESSOR <xenteze@gmail.com>
2018-06-06 00:11:02 +0300

    Линтовка

--- utils/Detector.js
+++ utils/Detector.js
@@ -5,74 +5,77 @@
 
 var Detector = {
 
-	canvas: !! window.CanvasRenderingContext2D,
-	webgl: ( function () {
+    "canvas": !!window.CanvasRenderingContext2D,
+    "webgl": (function () {
 
-		try {
+        try {
 
-			var canvas = document.createElement( 'canvas' ); return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
+            const canvas = document.createElement('canvas'); 
 
-		} catch ( e ) {
+            return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
 
-			return false;
+        } catch (e) {
 
-		}
+            return false;
 
-	} )(),
-	workers: !! window.Worker,
-	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+        }
 
-	getWebGLErrorMessage: function () {
+    }()),
+    "workers": !!window.Worker,
+    "fileapi": window.File && window.FileReader && window.FileList && window.Blob,
 
-		var element = document.createElement( 'div' );
-		element.id = 'webgl-error-message';
-		element.style.fontFamily = 'monospace';
-		element.style.fontSize = '13px';
-		element.style.fontWeight = 'normal';
-		element.style.textAlign = 'center';
-		element.style.background = '#fff';
-		element.style.color = '#000';
-		element.style.padding = '1.5em';
-		element.style.width = '400px';
-		element.style.margin = '5em auto 0';
+    "getWebGLErrorMessage": function () {
 
-		if ( ! this.webgl ) {
+        const element = document.createElement('div');
 
-			element.innerHTML = window.WebGLRenderingContext ? [
-				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-			].join( '\n' ) : [
-				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
-				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-			].join( '\n' );
+        element.id = 'webgl-error-message';
+        element.style.fontFamily = 'monospace';
+        element.style.fontSize = '13px';
+        element.style.fontWeight = 'normal';
+        element.style.textAlign = 'center';
+        element.style.background = '#fff';
+        element.style.color = '#000';
+        element.style.padding = '1.5em';
+        element.style.width = '400px';
+        element.style.margin = '5em auto 0';
 
-		}
+        if (!this.webgl) {
 
-		return element;
+            element.innerHTML = window.WebGLRenderingContext ? [
+                'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+                'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+            ].join('\n') : [
+                'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+                'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+            ].join('\n');
 
-	},
+        }
 
-	addGetWebGLMessage: function ( parameters ) {
+        return element;
 
-		var parent, id, element;
+    },
 
-		parameters = parameters || {};
+    "addGetWebGLMessage": function (parameters) {
 
-		parent = parameters.parent !== undefined ? parameters.parent : document.body;
-		id = parameters.id !== undefined ? parameters.id : 'oldie';
+        let parent, id, element;
 
-		element = Detector.getWebGLErrorMessage();
-		element.id = id;
+        parameters = parameters || {};
 
-		parent.appendChild( element );
+        parent = parameters.parent !== undefined ? parameters.parent : document.body;
+        id = parameters.id !== undefined ? parameters.id : 'oldie';
 
-	}
+        element = Detector.getWebGLErrorMessage();
+        element.id = id;
+
+        parent.appendChild(element);
+
+    }
 
 };
 
 // browserify support
-if ( typeof module === 'object' ) {
+if (typeof module === 'object') {
 
-	module.exports = Detector;
+    module.exports = Detector;
 
 }
